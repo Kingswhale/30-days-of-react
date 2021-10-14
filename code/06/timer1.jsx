2fcfff5e7e492a0128f8500c9ef842d163cef9a0
kingswhale <hardeyreemyarfyc@gmail.com>
2021-10-14 18:07:33 +0100

    update

--- code/06/timer1.jsx
+++ code/06/timer1.jsx
@@ -1,52 +1,47 @@
 export class Clock extends React.Component {
-
   constructor(props) {
-    super(props);
-    this.state = this.getTime();
+    super(props)
+    this.state = this.getTime()
   }
 
   componentDidMount() {
-    this.setTimer();
+    this.setTimer()
   }
 
   componentWillUnmount() {
     if (this.timeout) {
-      clearTimeout(this.timeout);
+      clearTimeout(this.timeout)
     }
   }
 
   setTimer() {
-    clearTimeout(this.timeout);
-    this.timeout = setTimeout(this.updateClock.bind(this), 1000);
+    clearTimeout(this.timeout)
+    this.timeout = setTimeout(this.updateClock.bind(this), 1000)
   }
 
   updateClock() {
-    this.setState(this.getTime, this.setTimer);
+    this.setState(this.getTime, this.setTimer)
   }
 
+  // getTime update the timing when the state is call.
+
   getTime() {
-    const currentTime = new Date();
+    const currentTime = new Date()
     return {
       hours: currentTime.getHours(),
       minutes: currentTime.getMinutes(),
       seconds: currentTime.getSeconds(),
-      ampm: currentTime.getHours() >= 12 ? 'pm' : 'am'
+      ampm: currentTime.getHours() >= 12 ? 'pm' : 'am',
     }
   }
 
   render() {
-    const {hours, minutes, seconds, ampm} = this.state;
+    const { hours, minutes, seconds, ampm } = this.state
     return (
-      <div className="clock">
-        {
-          hours == 0 ? 12 :
-            (hours > 12) ?
-              hours - 12 : hours
-        }:{
-          minutes > 9 ? minutes : `0${minutes}`
-        }:{
-          seconds > 9 ? seconds : `0${seconds}`
-        } {ampm}
+      <div className='clock'>
+        {hours == 0 ? 12 : hours > 12 ? hours - 12 : hours}:
+        {minutes > 9 ? minutes : `0${minutes}`}:
+        {seconds > 9 ? seconds : `0${seconds}`} {ampm}
       </div>
     )
   }
